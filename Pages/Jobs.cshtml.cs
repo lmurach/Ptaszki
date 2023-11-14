e2b5cea75340dd45fa3fec0376e987b5da291e23
Quillington <lauren.murach@gmail.com>
2023-11-13 20:44:16 -0500

    jobs page done

--- Pages/Jobs.cshtml.cs
+++ Pages/Jobs.cshtml.cs
@@ -1,3 +1,5 @@
+using BirdGame.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,10 +7,20 @@ namespace BirdGame.Pages;
 
 public class JobsModel : PageModel
 {
+    private readonly BirdDbContext _context;
     private readonly ILogger<JobsModel> _logger;
 
-    public JobsModel(ILogger<JobsModel> logger)
+    public ICollection<Bird> OwnedBirds { get; set; } = default!;
+
+    public JobsModel(BirdDbContext context, ILogger<JobsModel> logger)
     {
+        _context = context;
         _logger = logger;
+    }
+
+    public async Task OnGetAsync()
+    {
+        this.OwnedBirds = await _context.Birds
+            .ToListAsync();
     }
 }
