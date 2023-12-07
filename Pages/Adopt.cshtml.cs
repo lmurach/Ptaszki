e6fc809a5028996a2f7735625fd4825a4c6a383a
Quillington <lauren.murach@gmail.com>
2023-12-07 17:34:50 -0500

    reroll works

--- Pages/Adopt.cshtml.cs
+++ Pages/Adopt.cshtml.cs
@@ -13,7 +13,8 @@ public class AdoptModel : PageModel
     private readonly BirdDbContext _context;
     private readonly ILogger<AdoptModel> _logger;
 
-    public UserGame UserGameEntity = new UserGame();
+    [BindProperty]
+    public UserGame UserGameEntity { get; set; }
 
     public AdoptModel(BirdDbContext context, ILogger<AdoptModel> logger)
     {
@@ -34,16 +35,59 @@ public class AdoptModel : PageModel
             .SingleAsync();
     }
 
-    public IActionResult OnPostReroll(UserGame ug) {
-        Console.WriteLine(ug.Id);
+    public IActionResult OnPostReroll() {
         Console.WriteLine(UserGameEntity.Id);
-        // if (UserGameEntity.Seeds >= 2) {
-        //     UserGameEntity.Seeds -= 2;
-        //     _context.UserGames.Update(UserGameEntity);
-        //     _context.SaveChanges();
-        //     // Reroll();
-        //     return Page();
-        // }
-        return Page();
+        if (UserGameEntity.Seeds >= 2) {
+            UserGameEntity.Seeds -= 2;
+            _context.Entry(UserGameEntity).State = EntityState.Modified;
+            _context.SaveChanges();
+            Reroll();
+            return Redirect("./Adopt");
+        }
+        return Redirect("./Adopt");
+    }
+
+    public void Reroll() {
+        assignNewSSBirds();
+    }
+
+    int getRarity() {
+        var rand = new Random();
+        int roll = rand.Next(81);
+        roll += UserGameEntity.RarityUpgrade * (50 / (UserGameEntity.RarityUpgrade + 1));
+        
+        if (roll < 50) {
+            return 0;
+        }
+        if (roll < 75) {
+            return 1;
+        }
+        if (roll < 100) {
+            return 2;
+        }
+        if (roll < 111) {
+            return 3;
+        }
+        return 4;
+    }
+
+    Bird getBird(int rarity) {
+        List<Bird> birds = _context.Birds
+            .Where(b => b.Rarity == rarity)
+            .ToList();
+        var rand = new Random();
+        int bIndex = rand.Next(birds.Count);
+        return birds[bIndex];
+    }
+
+    void assignNewSSBirds() {
+        _context.RolledSSBs.RemoveRange(_context.RolledSSBs
+            .Where(b => b.User.Id == UserGameEntity.Id));
+        _context.SaveChanges();
+        for (int i = 0; i < 3; i++) {
+            Bird newBird = getBird(getRarity());
+            _context.Add(new RolledSSB {User = UserGameEntity, Bird = newBird});
+        }
+        _context.SaveChanges();
     }
 }
--- SQL_stuff/birds.sql
+++ SQL_stuff/birds.sql
@@ -20,3 +20,18 @@
 
 -- INSERT INTO Birds (Id, Name, Rarity, Description, Strength, Perception, Hunting)
 -- VALUES (6, "Blue Jay", 1, "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", 2, 2, 3);
+
+-- INSERT INTO Birds (Id, Name, Rarity, Description, Strength, Perception, Hunting)
+-- VALUES (7, "Snowy Egret", 1, "xxxxxxxxxxxxxxxxxxxxxxx", 3, 3, 1);
+
+
+-- INSERT INTO Birds (Id, Name, Rarity, Description, Strength, Perception, Hunting)
+-- VALUES (8, "Barn Owl", 2, "xxxxxxxxxxxxxxxxxxxxxxx", 2, 4, 5);
+
+
+-- INSERT INTO Birds (Id, Name, Rarity, Description, Strength, Perception, Hunting)
+-- VALUES (9, "European Bee Eater", 3, "xxxxxxxxxxxxxxxxxxxxxxx", 2, 5, 9);
+
+
+-- INSERT INTO Birds (Id, Name, Rarity, Description, Strength, Perception, Hunting)
+-- VALUES (10, "American Flamingo", 4, "xxxxxxxxxxxxxxxxxxxxxxx", 9, 9, 9);
